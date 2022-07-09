2d0b527f3c2a883f3f91adfe45f25a7b48b11f78
NecroBread <mariovyordanov@gmail.com>
2022-07-09 11:26:21 +0300

    Add tooltips to nav

--- src/components/navigation/NavList.js
+++ src/components/navigation/NavList.js
@@ -5,33 +5,35 @@ import { AiOutlineInfoCircle } from 'react-icons/ai'
 
 export default function NavList({ isMobile }) {
 
+	const tooltipClasses = isMobile ? '' : 'tooltip tooltip-bottom';
+
 	return (
 		<>
 			<li>
-				<NavLink to="/">
+				<NavLink to="/" className={tooltipClasses} data-tip='Home'>
 					<BsHouseDoor size={'1.5rem'} />
 					{isMobile ? 'Home' : ''}
 				</NavLink>
 			</li>
 			<li>
-				<NavLink to="/questions">
+				<NavLink to="/questions" className={tooltipClasses} data-tip='Questions'>
 					<BsCardChecklist size={'1.5rem'} />
 					{isMobile ? 'Questions' : ''}
 				</NavLink>
 			</li>
 			<li>
-				<NavLink to="/tags">
+				<NavLink to="/tags" className={tooltipClasses} data-tip='Tags'>
 					<BsBookmarks size={'1.5rem'} />
 					{isMobile ? 'Tags' : ''}
 				</NavLink>
 			</li>
 			<li>
-				<NavLink to="/profile">
+				<NavLink to="/profile" className={tooltipClasses} data-tip='Profile'>
 					<BsPerson size={'1.5rem'} />
 					{isMobile ? 'Profile' : ''}
 				</NavLink></li>
 			<li>
-				<NavLink to="/about">
+				<NavLink to="/about" className={tooltipClasses} data-tip='About'>
 					<AiOutlineInfoCircle size={'1.5rem'} />
 					{isMobile ? 'About' : ''}
 				</NavLink></li>
