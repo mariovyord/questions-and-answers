c3b38d6ed26e4c1f73a41c45587a71a20bb8272e
NecroBread <mariovyordanov@gmail.com>
2022-07-13 15:58:04 +0300

    Add sort functionality

--- src/components/common/SortSelect.js
+++ src/components/common/SortSelect.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-export default function SortSelect() {
+export default function SortSelect({ handleSort }) {
 	return (
 		<div className='w-full bg-base-100 shadow px-4 py-2 rounded-lg'>
 			<div>
 				<span className='font-bold text-sm mr-4'>Sort by:</span>
-				<select className="select select-sm w-full max-w-xs">
-					<option>Most upvoted</option>
-					<option>Most recent</option>
+				<select className="select select-sm w-full max-w-xs" onChange={handleSort}>
+					<option value={'highest-score'}>Highest score</option>
+					<option value={'most-recent'}>Most recent</option>
 				</select>
 			</div>
 		</div>
