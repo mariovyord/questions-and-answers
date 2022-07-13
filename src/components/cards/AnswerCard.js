17309d99635bd028b287e648619945d3b579af56
NecroBread <mariovyordanov@gmail.com>
2022-07-13 15:58:15 +0300

    Add data to answer card

--- src/components/cards/AnswerCard.js
+++ src/components/cards/AnswerCard.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { BsArrowDownCircle, BsArrowUpCircle } from 'react-icons/bs'
+import { BsArrowDownCircle, BsArrowUpCircle } from 'react-icons/bs';
+import MDEditor from '@uiw/react-md-editor';
 
-export default function AnswerCard() {
+export default function AnswerCard({ answer }) {
 	return (
 		<div className='w-full bg-base-100 shadow p-4 rounded-lg'>
 
@@ -14,26 +15,23 @@ export default function AnswerCard() {
 					</div>
 				</div>
 				<span className='font-bold flex ml-3'>
-					<Link to="/user/1234" className='place-self-center link-hover'>Mario Yordanov</Link>
+					<Link to={'/users/' + answer.owner._id} className='place-self-center link-hover'>{answer.owner.firstName} {answer.owner.lastName}</Link>
 				</span>
 			</div>
 
 			{/* Question */}
 			<div className='py-2'>
 				<div className='font-bold'>
-					<Link to="/question/1234" className='link-hover'>When did England become an island?</Link>
+					<Link to={"/questions/" + answer.parent._id} className='link-hover'>{answer.parent.body}</Link>
 				</div>
 				<div>
-					<span className='text-xs text-gray-500'>Tags: <Link to="" className='link-hover'>history</Link>, <Link to="" className='link-hover'>geography</Link></span>
+					<span className='text-xs text-gray-500'>Circle: <Link to={"/circles/" + answer.parent.parent} className='link-hover'>{answer.meta.circle}</Link></span>
 				</div>
 			</div>
 
 			{/* Answer */}
 			<div>
-				<p className='mb-1'>England isn't an island, but Britain is.</p>
-				<p className='mb-1'>It didn't used to be, it used to be part of a region called Doggerland (yes, really):</p>
-				<img className='mb-1' src="https://qph.fs.quoracdn.net/main-qimg-ee5cf0ccc770bba8093a9df564b6dd74-pjlq" alt="Map" />
-				<p className='mb-1'>It became an island about 6500 BCE, while the island of what is now Dogger Bank lasted another 1000 years or so after that. It’s not a happy thought to wonder what happened to any people living there at the time.</p>
+				<MDEditor.Markdown source={answer.body} style={{ whiteSpace: 'pre-wrap' }} />
 			</div>
 
 			{/* Menu buttons */}
