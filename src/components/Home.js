3ad2342b4c6b94de705897f8f74d5074fa350872
NecroBread <mariovyordanov@gmail.com>
2022-07-13 15:57:46 +0300

    Hide sidebars on small screen and sort option

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import AnswerCard from './cards/AnswerCard';
 import Stats from './sidebar/Stats';
 import CirlclesList from './sidebar/CirclesList';
@@ -6,23 +6,61 @@ import SortSelect from './common/SortSelect';
 import { themeChange } from 'theme-change';
 
 export default function Home() {
+
+	const [answers, setAnswers] = useState([]);
+	const [sort, setSort] = useState('sortBy=score%20desc')
+	const [isDesktop, setDesktop] = useState(window.innerWidth > 768);
+
+	const updateMedia = () => {
+		setDesktop(window.innerWidth > 768);
+	};
+
+	useEffect(() => {
+		window.addEventListener("resize", updateMedia);
+		return () => window.removeEventListener("resize", updateMedia);
+	});
+
 	useEffect(() => {
 		themeChange(false);
-	}, [])
+
+		(async function () {
+			const data = await fetch(`http://localhost:3030/api/collections/answers?${sort}&populate=owner parent`);
+			const res = await data.json();
+			setAnswers(res.result);
+		})()
+
+	}, [sort]);
+
+	const handleSort = (e) => {
+		const sort = e.target.value;
+		console.log(sort)
+		if (sort === 'most-recent') {
+			setSort('sortBy=createdAt%20desc')
+		} else {
+			setSort('sortBy=score%20desc')
+		}
+	}
+
+
 	return (
-		<div className='grid grid-cols-5 gap-2 max-w-5xl py-2'>
-			{/* Sidebar */}
-			<div className='col-span-1'>
-				<CirlclesList />
-			</div>
+		<div className='grid grid-cols-5 gap-2 max-w-5xl p-2'>
+
+			{/* Sidebar Left */}
+			{isDesktop
+				? <div className='col-span-1'>
+					<CirlclesList />
+				</div>
+				: ''}
+
 			{/* Main Feed */}
-			<div className='col-span-3 grid gap-2'>
-				<SortSelect />
-				<AnswerCard />
-				<AnswerCard />
-				<AnswerCard />
+			<div className='col-span-5 md:col-span-3 grid gap-2'>
+				<SortSelect handleSort={handleSort} />
+				{answers.map(x => <AnswerCard key={x._id} answer={x} />)}
+
 			</div>
-			<div className='col-span-1'>
+
+			{/* Sidebar Right */}
+			<div className={isDesktop ? 'col-span-1' : 'hidden'}>
 				<div>
 					<h3 className='text-center font-bold'>Select theme</h3>
 					<div className='flex justify-around py-2'>
