040bfc319d9c6d2804efdc3cdee96572153ba1b7
NecroBread <mariovyordanov@gmail.com>
2022-07-14 15:34:12 +0300

    Add fetch to stats sidebar

--- src/components/feautures/Stats.js
+++ src/components/feautures/Stats.js
@@ -1,25 +1,28 @@
-import React from 'react'
+import React from 'react';
+import useFetch from '../hooks/useFetch';
 
 export default function Stats() {
+
+	const { data: answersCount } = useFetch(`/collections/answers?count=true`);
+	const { data: questionsCount } = useFetch(`/collections/questions?count=true`);
+	const { data: usersCount } = useFetch(`/collections/users?count=true`);
+
+
 	return (
 		<div className="stats stats-vertical bg-base-200 w-full">
-
 			<div className="stat">
 				<div className="stat-title">Questions</div>
-				<div className="stat-value">31K</div>
-				<div className="stat-desc">Jan 1st - Feb 1st</div>
+				<div className="stat-value">{questionsCount || '&infin;'}</div>
 			</div>
 
 			<div className="stat">
 				<div className="stat-title">Answers</div>
-				<div className="stat-value">4,200</div>
-				<div className="stat-desc">↗︎ 400 (22%)</div>
+				<div className="stat-value">{answersCount || '&infin;'}</div>
 			</div>
 
 			<div className="stat">
 				<div className="stat-title">Users</div>
-				<div className="stat-value">1,200</div>
-				<div className="stat-desc">↘︎ 90 (14%)</div>
+				<div className="stat-value">{usersCount || '&infin;'}</div>
 			</div>
 
 		</div>
