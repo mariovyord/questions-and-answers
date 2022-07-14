f6bc50884fe4e8ce5f253ab06b8b56f5ab7f93fa
NecroBread <mariovyordanov@gmail.com>
2022-07-14 10:52:38 +0300

    Add utils folder and circles list refactor

--- src/components/feautures/CirclesList.js
+++ src/components/feautures/CirclesList.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-
-const tagsMockup = ['history', 'programming', 'biology', 'hollywood', 'photography', 'geography', 'guns', 'comedy', 'movies', 'horror books', 'world politics', 'flying', 'cooking']
+import { toTitleCase } from '../../utils/utils';
 
 export default function CirlclesList() {
 
-	const toTitleCase = (str) => {
-		return str.replace(
-			/\w\S*/g,
-			function (txt) {
-				return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-			}
-		);
-	}
-
-	// Add data service
-	const tags = tagsMockup;
+	const mainCircles = [
+		'technology',
+		'programming',
+		'science',
+		'history',
+		'books',
+		'movies',
+		'world politics',
+		'economics',
+	]
 
 	return (
-		<div className='p-3 '>
+		<div className='p-3'>
 			<h2 className='text-2xl font-bold'>Circles</h2>
 			<ul>
-				{tags.map(x => <li className='py-1 link link-hover' key={x}><Link to='/'>{toTitleCase(x)}</Link></li >)}
+				{mainCircles.map(x => {
+					const url = `/circles/${x}`
+					return <li className='py-1 link link-hover' key={x}><Link to={url}>{toTitleCase(x)}</Link></li >
+				})}
 			</ul>
 		</div>
 	)
