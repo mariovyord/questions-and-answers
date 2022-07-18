8e6d78e73004ff06ac8c2dc7468ab6894f54f57b
NecroBread <mariovyordanov@gmail.com>
2022-07-18 11:24:53 +0300

    Add query persist btw page changes

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { useParams, useSearchParams, useNavigate } from 'react-router-dom';
+import { useParams, useSearchParams, useNavigate, useLocation } from 'react-router-dom';
 import FeedOptions from './FeedOptions';
 import AnswerCard from '../cards/AnswerCard/AnswerCard';
 import useFetch from '../hooks/useFetch';
@@ -7,22 +7,17 @@ import { PAGE_SIZE } from '../../constants';
 
 export default function Feed({ urlOptions }) {
 	let { pageParam } = useParams();
-	const [offset, setOffset] = useState(0)
 	const [query, setQuery] = useSearchParams();
 	const navigate = useNavigate();
+	const location = useLocation();
 
 	// TODO Add error handling!
-	const { data, loading, error } = useFetch(`/collections/answers?${query.get('sortBy') || 'sortBy=score%20desc'}&offset=${offset}&pageSize=${PAGE_SIZE}}&populate=owner${urlOptions}`)
+	const { data, loading, error } = useFetch(`/collections/answers?${query.get('sortBy') || 'sortBy=score%20desc'}&page=${pageParam || 1}&pageSize=${PAGE_SIZE}}&populate=owner${urlOptions}`)
 	const { data: docsCount } = useFetch(`/collections/answers?count=true`);
-
-	useEffect(() => {
-		if (pageParam) {
-			setOffset((parseInt(pageParam) - 1) * PAGE_SIZE);
-		}
-	}, [pageParam]);
 
 	const handleSort = (e) => {
 		const sort = e.target.value;
+		console.log(sort)
 		if (sort === 'most-recent') {
 			setQuery({ 'sortBy': 'sortBy=createdAt%20desc' });
 		} else {
@@ -32,19 +27,20 @@ export default function Feed({ urlOptions }) {
 
 	const handlePage = (changeNum) => {
 		if (!pageParam) {
-			return navigate('/' + 1);
+			return navigate(`/2${location.search}`);
 		};
-		navigate(`/${parseInt(pageParam) + changeNum}`);
+		navigate('/' + (parseInt(pageParam) + changeNum) + location.search);
 	}
 
 	return (
 		<>
 			{loading
+				// TODO Add Loading spinner and stuff
 				? <div className='col-span-5 md:col-span-3 grid gap-2 h-screen bg-base-100 rounded-lg'>
 					< FeedOptions handleSort={handleSort} />
 				</div >
 				: <div className='col-span-5 md:col-span-3 grid gap-2'>
-					<FeedOptions handleSort={handleSort} handlePage={handlePage} page={pageParam} docsCount={docsCount} />
+					<FeedOptions handleSort={handleSort} handlePage={handlePage} page={pageParam} docsCount={docsCount} sort={query.get('sortBy')} />
 					{data.map(x => <AnswerCard key={x._id} answer={x} />)}
 					<FeedOptions handleSort={handleSort} handlePage={handlePage} page={pageParam} docsCount={docsCount} />
 				</div>
--- src/components/feed/FeedOptions.js
+++ src/components/feed/FeedOptions.js
@@ -8,7 +8,7 @@ export default function FeedOptions({ handleSort, handlePage, page, docsCount })
 		<div className="btn-group grid grid-cols-3 gap-2" >
 			<button className="btn btn-outline" disabled={page <= 1} onClick={() => handlePage(-1)}>Previous page</button>
 			<select className="select w-full max-w-xs btn-outline" onChange={handleSort}>
-				<option value={'highest-score'}>Sort by most stars</option>
+				<option value={'score'}>Sort by score</option>
 				<option value={'most-recent'}>Sort by most recent</option>
 			</select>
 			<button className="btn btn-outline" disabled={page >= Math.ceil(docsCount / PAGE_SIZE)} onClick={() => handlePage(1)}>Next Page</button>
