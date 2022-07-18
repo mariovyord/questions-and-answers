e9ff8e826702628fcc90f2164a9d40edced23069
NecroBread <mariovyordanov@gmail.com>
2022-07-18 13:50:37 +0300

    Change pagination from url to query

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -1,36 +1,40 @@
 import React from 'react';
-import { useParams, useSearchParams, useNavigate, useLocation } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import FeedOptions from './FeedOptions';
 import AnswerCard from '../cards/AnswerCard/AnswerCard';
 import useFetch from '../hooks/useFetch';
 import { PAGE_SIZE } from '../../constants';
 
 export default function Feed({ urlOptions = '' }) {
-	let { pageParam } = useParams();
 	const [query, setQuery] = useSearchParams();
-	const navigate = useNavigate();
-	const location = useLocation();
 
 	// TODO Add error handling!
 	const { data, loading, error } = useFetch(`/collections/answers?${query.get('sortBy')
 		? 'sortBy=' + query.get('sortBy')
-		: 'sortBy=score%20desc'}&page=${pageParam || 1}&pageSize=${PAGE_SIZE}&populate=owner${urlOptions}`)
+		: 'sortBy=score%20desc'}&page=${query.get('page') || 1}&pageSize=${PAGE_SIZE}&populate=owner${urlOptions}`)
 	const { data: docsCount } = useFetch(`/collections/answers?count=true`);
+
+	const handleQuery = (page, sortBy) => setQuery({
+		page: page || 1,
+		sortBy: sortBy || 'score%20desc',
+	})
 
 	const handleSort = (e) => {
 		const sort = e.target.value;
+		const page = query.get('page');
 		if (sort === 'most-recent') {
-			setQuery({ 'sortBy': 'createdAt%20desc' });
+			handleQuery(page, 'createdAt%20desc');
 		} else {
-			setQuery({ 'sortBy': 'score%20desc' });
+			handleQuery(page);
 		}
 	}
 
 	const handlePage = (changeNum) => {
-		if (!pageParam) {
-			return navigate(`/2${location.search}`);
-		};
-		navigate('/' + (parseInt(pageParam) + changeNum) + location.search);
+		let page = query.get('page');
+		if (page) page = parseInt(page) + changeNum;
+		if (!page) page = 1 + changeNum;
+		const sort = query.get('sortBy');
+		handleQuery(page, sort);
 	}
 
 	return (
@@ -41,9 +45,9 @@ export default function Feed({ urlOptions = '' }) {
 					< FeedOptions handleSort={handleSort} />
 				</div >
 				: <div className='col-span-5 md:col-span-3 grid gap-2'>
-					<FeedOptions handleSort={handleSort} handlePage={handlePage} page={pageParam} docsCount={docsCount} sort={query.get('sortBy')} />
+					<FeedOptions handleSort={handleSort} handlePage={handlePage} page={query.get('page')} docsCount={docsCount} sort={query.get('sortBy')} />
 					{data.map(x => <AnswerCard key={x._id} answer={x} />)}
-					<FeedOptions handleSort={handleSort} handlePage={handlePage} page={pageParam} docsCount={docsCount} />
+					<FeedOptions handleSort={handleSort} handlePage={handlePage} page={query.get('page')} docsCount={docsCount} />
 				</div>
 			}
 		</>)
