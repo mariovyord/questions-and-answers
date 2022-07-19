316b7848760e1b15b2dc6b7499d558c99befb4ce
NecroBread <mariovyordanov@gmail.com>
2022-07-19 10:54:00 +0300

    Fix

--- src/components/utils/FeedOptionsContainer.js
+++ src/components/utils/FeedOptionsContainer.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
-import { PAGE_SIZE } from '../../constants';
-
-export default function FeedOptionsContainer({ handleSort, handlePage, page, docsCount, children }) {
+export default function FeedOptionsContainer({ handlePage, page, pageSize, docsCount, children }) {
 	if (page === undefined) page = 1;
 
 	return (
 		<div className="btn-group grid grid-cols-3 gap-2" >
 			<button className="btn btn-outline" disabled={page <= 1} onClick={() => handlePage(-1)}>Prev page</button>
 			{children}
-			<button className="btn btn-outline" disabled={page >= Math.ceil(docsCount / PAGE_SIZE)} onClick={() => handlePage(1)}>Next Page</button>
+			<button className="btn btn-outline" disabled={page >= Math.ceil(docsCount / pageSize)} onClick={() => handlePage(1)}>Next Page</button>
 		</div >
 	)
 }
