14354b65fd34cec6761d0cd9701c0a03903ed494
NecroBread <mariovyordanov@gmail.com>
2022-07-19 10:53:40 +0300

    Fix query

--- src/components/feed/Feed.js
+++ src/components/feed/Feed.js
@@ -2,16 +2,16 @@ import React from 'react';
 import { useSearchParams } from 'react-router-dom';
 import AnswerCard from './AnswerCard/AnswerCard';
 import useFetch from '../hooks/useFetch';
-import { PAGE_SIZE } from '../../constants';
 import FeedOptionsContainer from '../utils/FeedOptionsContainer';
 
 export default function Feed({ urlOptions = '' }) {
+	const pageSize = 2;
 	const [query, setQuery] = useSearchParams();
 
 	// TODO Add error handling!
 	const { data, loading, error } = useFetch(`/collections/answers?${query.get('sortBy')
 		? 'sortBy=' + query.get('sortBy')
-		: 'sortBy=score%20desc'}&page=${query.get('page') || 1}&pageSize=${PAGE_SIZE}&populate=owner${urlOptions}`)
+		: 'sortBy=score%20desc'}&page=${query.get('page') || 1}&pageSize=${pageSize}&populate=owner${urlOptions}`)
 	const { data: docsCount } = useFetch(`/collections/answers?count=true${urlOptions}`);
 
 	const handleQuery = (page, sortBy) => setQuery({
@@ -22,7 +22,7 @@ export default function Feed({ urlOptions = '' }) {
 	const handleSort = (e) => {
 		const sort = e.target.value;
 		const page = query.get('page');
-		if (sort === 'most-recent') {
+		if (sort === 'createdAt%20desc') {
 			handleQuery(page, 'createdAt%20desc');
 		} else {
 			handleQuery(page);
@@ -42,25 +42,42 @@ export default function Feed({ urlOptions = '' }) {
 			{loading
 				// TODO Add Loading spinner and stuff
 				? <div className='col-span-5 md:col-span-3 grid gap-2 h-screen rounded-lg w-full'>
-					<FeedOptionsContainer handlePage={handlePage} page={query.get('page')} docsCount={docsCount} sort={query.get('sortBy')} >
-						<select className="select w-full max-w-xs btn-outline" onChange={handleSort}>
-							<option value={'score'}>Sort by score</option>
-							<option value={'most-recent'}>Sort by most recent</option>
-						</select>
-					</FeedOptionsContainer>
+
 				</div >
 				: <div className='col-span-5 md:col-span-3 grid gap-2'>
-					<FeedOptionsContainer handlePage={handlePage} page={query.get('page')} docsCount={docsCount} sort={query.get('sortBy')} >
-						<select className="select w-full max-w-xs btn-outline" onChange={handleSort}>
-							<option value={'score'}>Sort by score</option>
-							<option value={'most-recent'}>Sort by most recent</option>
+					<FeedOptionsContainer
+						handlePage={handlePage}
+						page={query.get('page')}
+						pageSize={pageSize}
+						docsCount={docsCount}
+						sort={query.get('sortBy')}
+					>
+						<select
+							className="select w-full max-w-xs btn-outline"
+							value={query.get('sortBy') || 'score%20desc'}
+							onChange={handleSort}
+						>
+							<option value={'score%20desc'}>Sort by score</option>
+							<option value={'createdAt%20desc'}>Sort by most recent</option>
 						</select>
 					</FeedOptionsContainer>
+
 					{data.map(x => <AnswerCard key={x._id} answer={x} />)}
-					<FeedOptionsContainer handlePage={handlePage} page={query.get('page')} docsCount={docsCount} sort={query.get('sortBy')} >
-						<select className="select w-full max-w-xs btn-outline" onChange={handleSort}>
-							<option value={'score'}>Sort by score</option>
-							<option value={'most-recent'}>Sort by most recent</option>
+
+					<FeedOptionsContainer
+						handlePage={handlePage}
+						page={query.get('page')}
+						pageSize={pageSize}
+						docsCount={docsCount}
+						sort={query.get('sortBy')}
+					>
+						<select
+							className="select w-full max-w-xs btn-outline"
+							value={query.get('sortBy') || 'score%20desc'}
+							onChange={handleSort}
+						>
+							<option value={'score%20desc'}>Sort by score</option>
+							<option value={'createdAt%20desc'}>Sort by most recent</option>
 						</select>
 					</FeedOptionsContainer>
 				</div>
