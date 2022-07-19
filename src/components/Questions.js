14c01aa3accea20e6cd9be51135a1e7bb970ce57
NecroBread <mariovyordanov@gmail.com>
2022-07-19 10:53:30 +0300

    Add sort by category

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -4,39 +4,39 @@ import Stats from './feautures/Stats';
 import CirlclesList from './feautures/CirclesList';
 import QuestionCard from './cards/QuestionCard';
 import useFetch from './hooks/useFetch';
-import { themeChange } from 'theme-change';
 import FeedOptionsContainer from './utils/FeedOptionsContainer';
 
-
 export default function Questions({ questions }) {
+	const pageSize = 3;
+
 	const [isDesktop, setDesktop] = useState(window.innerWidth > 768);
 	const [query, setQuery] = useSearchParams();
 
+	const { data, loading, error } = useFetch(`/collections/questions?sortBy=createdAt%20desc&page=${query.get('page') || 1}&pageSize=${pageSize}${query.get('where') ? `&where=${query.get('where')}` : ''}`);
+	const { data: ciclesData, loading: loadingCircles, error: errorCircles } = useFetch(`/collections/circles`);
+	const { data: docsCount } = useFetch(`/collections/questions?${query.get('where') ? `where=${query.get('where')}&` : ''}count=true`);
+	console.log('data', data)
+	console.log('docsCount', docsCount)
 	useEffect(() => {
 		window.addEventListener("resize", updateMedia);
 		return () => window.removeEventListener("resize", updateMedia);
 	});
 
-	useEffect(() => {
-		// Needed for the theme change lib to work!
-		themeChange(false);
-	}, [isDesktop]);
-
 	const updateMedia = () => {
 		setDesktop(window.innerWidth > 768);
 	};
-	const handleQuery = (page, sortBy) => setQuery({
+	const handleQuery = (page, where) => setQuery({
 		page: page || 1,
-		sortBy: sortBy || 'score%20desc',
+		where: where || '',
 	})
 
 	const handleSort = (e) => {
-		const sort = e.target.value;
+		const whereId = e.target.value;
 		const page = query.get('page');
-		if (sort === 'most-recent') {
-			handleQuery(page, 'createdAt%20desc');
-		} else {
+		if (whereId === 'all') {
 			handleQuery(page);
+		} else {
+			handleQuery(page, `circle=${whereId}`);
 		}
 	}
 
@@ -44,12 +44,10 @@ export default function Questions({ questions }) {
 		let page = query.get('page');
 		if (page) page = parseInt(page) + changeNum;
 		if (!page) page = 1 + changeNum;
-		const sort = query.get('sortBy');
-		handleQuery(page, sort);
+		const where = query.get('where');
+		handleQuery(page, where);
 	}
 
-	const { data, loading, error } = useFetch(`/collections/questions`);
-	const { data: docsCount } = useFetch(`/collections/questions?count=true`);
 
 	return (
 		<div className='grid grid-cols-5 gap-2 max-w-5xl p-2 w-full'>
@@ -77,11 +75,25 @@ export default function Questions({ questions }) {
 					: <>
 						<div className='col-span-3 gap-2'>
 							<div className='grid gap-2'>
-								<FeedOptionsContainer>
-									<select className="select w-full max-w-xs btn-outline" onChange={handleSort}>
-										<option value={'score'}>All</option>
-										<option value={'score'}>History</option>
-										<option value={'most-recent'}>Programming</option>
+								<FeedOptionsContainer
+									handlePage={handlePage}
+									page={query.get('page')}
+									pageSize={pageSize}
+									docsCount={docsCount}
+								>
+									<select
+										className="select w-full max-w-xs btn-outline"
+										onChange={handleSort}
+										value={query.get('where')?.split('=')[1] || 'all'}
+									>
+										<option value="all">All</option>
+										{
+											loadingCircles
+												? null
+												: <>
+													{ciclesData.map(x => <option key={x._id} value={x._id}>{x.title}</option>)}
+												</>
+										}
 									</select>
 								</FeedOptionsContainer>
 								{/* TODO Change it to quiestions prop */}
@@ -92,12 +104,13 @@ export default function Questions({ questions }) {
 			}
 
 			{/* Sidebar Right */}
-			{isDesktop
-				? <div className={'col-span-1'}>
-					<Stats />
-				</div>
-				: null
+			{
+				isDesktop
+					? <div className={'col-span-1'}>
+						<Stats />
+					</div>
+					: null
 			}
-		</div>
+		</div >
 	)
 }
