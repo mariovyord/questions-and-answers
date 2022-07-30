ca7edde76657dacc4f4b97434babbfcf3dcd9a7a
NecroBread <mariovyordanov@gmail.com>
2022-07-30 17:27:37 +0300

    Add prod api url

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,5 @@
-export const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:3030/api';
+export const API_URL = process.env.NODE_ENV === 'production'
+	? 'https://questions-and-answers-rest.herokuapp.com/api'
+	: 'http://localhost:3030/api';
+
 
