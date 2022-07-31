bafa906d6b69a671e404b893c37e5a4ebcfb140c
NecroBread <mariovyordanov@gmail.com>
2022-07-31 11:09:40 +0300

    Dropdown menu closes if clicked again

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import NavList from './NavList';
 import { Link } from 'react-router-dom';
 import AddQuestionModal from './addQuestionModal/AddQuestionModal';
@@ -8,15 +8,23 @@ import { GiBlackBook } from 'react-icons/gi'
 export default function Navbar() {
 	const { userData, handleLogout } = useContext(AuthContext);
 
+	function checkAndCloseDropDown(e) {
+		let targetEl = e.currentTarget;
+		if (targetEl && targetEl.matches(':focus')) {
+			setTimeout(function () {
+				targetEl.blur();
+			}, 0);
+		}
+	}
 	return (
 		<div className='bg-base-100 flex justify-center'>
 			<div className="navbar max-w-5xl">
 				<div className="navbar-start">
 					<div className="dropdown">
-						<label tabIndex="0" className="btn btn-ghost lg:hidden">
+						<label tabIndex="0" className="btn btn-ghost lg:hidden" onMouseDown={(e) => checkAndCloseDropDown(e)}>
 							<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
 						</label>
-						<ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+						<ul onMouseDown={(e) => checkAndCloseDropDown(e)} tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
 							<NavList userData={userData} isMobile={true} />
 						</ul>
 					</div>
