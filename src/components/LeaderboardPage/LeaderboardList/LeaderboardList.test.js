84dbb65654f1a0be825843cee0fe9cef5e8fd09a
NecroBread <mariovyordanov@gmail.com>
2022-08-08 17:47:40 +0300

    Leaderboard list tests

---
+++ src/components/LeaderboardPage/LeaderboardList/LeaderboardList.test.js
@@ -0,0 +1,67 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import LeaderboardList from './LeaderboardList';
+
+const listOfUsers = [
+	{
+		"_id": "user1Id",
+		"totalScore": '6',
+		"user": {
+			"username": "user1",
+			"firstName": "User1",
+			"lastName": "Userson1"
+		}
+	},
+	{
+		"_id": "user2Id",
+		"totalScore": '5',
+		"user": {
+			"username": "user2",
+			"firstName": "User2",
+			"lastName": "Userson2"
+		}
+	},
+	{
+		"_id": "user3Id",
+		"totalScore": '4',
+		"user": {
+			"username": "user3",
+			"firstName": "User3",
+			"lastName": "Userson3"
+		}
+	},
+]
+
+describe('leaderboard', () => {
+	test('all rows appear in leaderboard', async () => {
+		render(
+			<LeaderboardList list={listOfUsers} />, { wrapper: MemoryRouter }
+		)
+		const allRows = screen.getAllByRole('row');
+		expect(allRows).toHaveLength(4);
+	})
+
+	test('user data appear', () => {
+		render(
+			<LeaderboardList list={listOfUsers} />, { wrapper: MemoryRouter }
+		)
+		const user1 = screen.getByRole('link', { name: 'User1 Userson1' })
+		expect(user1).toBeInTheDocument();
+
+		const user1Score = screen.getByRole('cell', { name: '6' });
+		expect(user1Score).toBeInTheDocument();
+	})
+
+	test('first user has proper hrefs on name and details btn', async () => {
+		render(
+			<LeaderboardList list={listOfUsers} />, { wrapper: MemoryRouter }
+		)
+
+		const user1name = screen.getByRole('link', { name: 'User1 Userson1' });
+		const user1DetailsBtn = screen.getAllByRole('link', { name: 'Details' })[0];
+
+		expect(user1name).toHaveAttribute('href', '/profile/user1Id')
+		expect(user1DetailsBtn).toHaveAttribute('href', '/profile/user1Id')
+	})
+})
--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,13 +1,36 @@
 import { rest } from 'msw';
 
-// TODO ...
 export const handlers = [
-	rest.get('http://localhost:3030/api', (req, res, ctx) => {
+	rest.get('http://localhost:3030/api/users/leaderboard', (req, res, ctx) => {
 		return res(
 			ctx.json([
 				{
-					'Prop': 'Prop',
-				}
+					"_id": "user1Id",
+					"totalScore": 3,
+					"user": {
+						"username": "user1",
+						"firstName": "User1",
+						"lastName": "Userson1"
+					}
+				},
+				{
+					"_id": "user2Id",
+					"totalScore": 2,
+					"user": {
+						"username": "user2",
+						"firstName": "User2",
+						"lastName": "Userson2"
+					}
+				},
+				{
+					"_id": "user3Id",
+					"totalScore": 1,
+					"user": {
+						"username": "user3",
+						"firstName": "User3",
+						"lastName": "Userson3"
+					}
+				},
 			])
 		)
 	})
