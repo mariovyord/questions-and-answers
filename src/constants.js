c52358a9e73b0269a85e8434de9140e0155a5842
NecroBread <mariovyordanov@gmail.com>
2022-08-15 14:55:22 +0300

    .

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,7 @@
-// export const API_URL = process.env.NODE_ENV === 'production'
-// 	? 'https://questions-and-answers-rest.herokuapp.com/api'
-// 	: 'http://localhost:3030/api';
+export const API_URL = process.env.NODE_ENV === 'production'
+	? 'https://questions-and-answers-rest.herokuapp.com/api'
+	: 'http://localhost:3030/api';
 
-export const API_URL = 'https://questions-and-answers-rest.herokuapp.com/api';
-// export const API_URL = 'http://localhost:3030/api';
 
 
 
